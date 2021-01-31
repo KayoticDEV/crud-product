8d6b5d8ce93fc7568721090f513c3c1ed19dc6ef
KayoticDEV <dev.harshit.1024@gmail.com>
2021-01-31 01:07:35 -0800

    Uploading Project Model Backend

---
+++ crud-product-backend/src/main/java/com/spartandevs/CrudProductBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spartandevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProductBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudProductBackendApplication.class, args);
+    }
+
+}
---
+++ crud-product-backend/src/test/java/com/spartandevs/CrudProductBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartandevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProductBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
